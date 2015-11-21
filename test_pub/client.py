49b98627187d72a084ba5f7122d0da19ca2d6994
franis <franis.sirkovic@gmail.com>
2015-11-21 23:03:26 +0100

    Publish client.

--- test_pub/client.py
+++ test_pub/client.py
@@ -8,29 +8,17 @@ def on_connect(client, userdata, rc):
 def on_disconnect(client, userdata, rc):
     print 'Disconnection result:', error_string(rc)
 
-def on_subscribe(client, userdata, mid, qos):
-    print 'Subscribe successful:', qos
-
-def on_unsubscribe(client, userdata, mid):
-    print 'Unubscribe successful:'
-
-def on_message(client, userdata, message):
-    print 'Received:', message.payload
-
 def start_receiver():
     mqttc = mqtt.Client()
     mqttc.on_connect = on_connect
     mqttc.on_disconnect = on_disconnect
-    mqttc.on_subscribe = on_subscribe
-    mqttc.on_unsubscribe = on_unsubscribe
-    mqttc.on_message = on_message
     mqttc.connect('localhost')
     mqttc.loop_start()
-    mqttc.subscribe('my/topic')
     s = ''
     while 'q' != s:
         s = raw_input('Enter "q" to exit:')
-    mqttc.unsubscribe('my/topic')
+        if 'p' == s:
+            mqttc.publish('my/topic', payload='trtmrt')
     mqttc.loop_stop()
     mqttc.disconnect()
     pass
